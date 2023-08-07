424d550e097e39cde3d41844343712e747544d6a
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-07 02:11:30 +0200

    event can be set

--- src/components/event-calendar.tsx
+++ src/components/event-calendar.tsx
@@ -3,23 +3,44 @@ import { useState } from 'react';
 import { Calendar } from "./ui/calendar";
 import { Button } from './ui/button';
 
+const padZeros = (num: number, digits = 2, pad = '0') => String(pad.repeat(digits) + num).slice(digits * -1);
+
 export function EventCalendar() {
 	const [date, setDate] = useState<Date | undefined>(new Date());
+	const [message, setMessage] = useState('');
+	const [eventCreated, setEventCreated] = useState(false);
 
 	const getFullDate = () => {
-		return 'nnn';
+		if (date) {
+			return `${date.getFullYear()}-${padZeros(date.getMonth() + 1)}-${padZeros(date.getDate())}`
+		} else {
+			return '';
+		}
+	}
+
+	const handleCreateEvent = () => {
+		setMessage(`Event for ${getFullDate()} was created.`);
+		setEventCreated(true);
 	}
 
 	return (
 		<>
-		<Calendar
-			mode="single"
-			selected={date}
-			onSelect={setDate}
-			className="rounded-md border bg-slate-200"
-		/>
-		<Button onClick={() => console.log('button clicked')}>Create Event</Button>
-		<div>{getFullDate()}</div>
+			<Calendar
+				mode="single"
+				selected={date}
+				disableNavigation={eventCreated}
+				onSelect={setDate}
+				disabled={eventCreated}
+				className="rounded-md border bg-yellow-400"
+			/>
+			{!eventCreated ? (
+				<>
+					<div>{getFullDate()}</div>
+					<Button onClick={() => handleCreateEvent()}>Create Event</Button>
+				</>
+			) : (
+				<div className='text-green-800 font-bold'>{message}</div>
+			)}
 		</>
 	)
 }
